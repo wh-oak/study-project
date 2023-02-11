6c885e6a012a1837d02d04de71207fa8c74a5d56
wh <2218350570@qq.com>
2023-02-12 02:35:41 +0800

    在家完善 B1 功能 并开发 B2 功能 B2

---
+++ B1.java
@@ -0,0 +1,5 @@
+public class B1 {
+    public static void main(String[] arg) {
+        System.out.print("在家开发 B 功能");
+    }
+}
---
+++ B2.java
@@ -0,0 +1,5 @@
+public class B2 {
+    public static void main(String[] arg) {
+        System.out.print("在家开发 B2 功能");
+    }
+}
